22166321696f61b0d4fefe8e3b3bb05ab71fcb63
DiEp2001 <110647097+DiEp2001@users.noreply.github.com>
2023-09-18 15:00:14 +0700

    diep

--- EP_Jewellery/Db/DatabaseContext.cs
+++ EP_Jewellery/Db/DatabaseContext.cs
@@ -1,67 +1,71 @@
 ﻿using EP_Jewellery.Models;
 using Microsoft.EntityFrameworkCore;
-
-public class DatabaseContext : DbContext
+namespace EP_Jewellery.Db
 {
-    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    public class DatabaseContext : DbContext
     {
-    }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
 
-    public DbSet<BrandMst> BrandMsts { get; set; }
-    public DbSet<CatMst> CatMsts { get; set; }
-    public DbSet<CertifyMst> CertifyMsts { get; set; }
-    public DbSet<DimMst> DimMsts { get; set; }
-    public DbSet<DimQltyMst> DimQltyMsts { get; set; }
-    public DbSet<GoldKrtMst> GoldKrtMsts { get; set; }
-    public DbSet<ProdMst> ProdMsts { get; set; }
-    public DbSet<StoneMst> StoneMsts { get; set; }
-    public DbSet<StoneQltyMst> StoneQltyMsts { get; set; }
-    public DbSet<ItemMst> Items { get; set; }
-    public DbSet<DimInfoMst> DimInfos { get; set; }
-    public DbSet<JewelTypeMst> JewelTypes { get; set; }
-    public DbSet<DimQltySubMst> DimQltySubMsts { get; set; }
-
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<ItemMst>()
-          .HasKey(i => i.Style_Code);
-
-        modelBuilder.Entity<ItemMst>()
-            .HasOne(i => i.Brand)
-            .WithMany()
-            .HasForeignKey(i => i.Brand_ID);
-
-        modelBuilder.Entity<ItemMst>()
-            .HasOne(i => i.Category)
-            .WithMany()
-            .HasForeignKey(i => i.Cat_ID);
-
-        modelBuilder.Entity<ItemMst>()
-            .HasOne(i => i.Certify)
-            .WithMany()
-            .HasForeignKey(i => i.Certify_ID);
-
-        modelBuilder.Entity<ItemMst>()
-            .HasOne(i => i.Product)
-            .WithMany()
-            .HasForeignKey(i => i.Prod_ID);
-
-        modelBuilder.Entity<ItemMst>()
-            .HasOne(i => i.GoldCarat)
-            .WithMany()
-            .HasForeignKey(i => i.GoldType_ID);
-        modelBuilder.Entity<DimMst>()
-            .HasOne(d => d.Item)
-            .WithMany(i => i.Dimensions)
-            .HasForeignKey(d => d.Style_Code);
+        public DbSet<Admin> Admin { get; set; }
+        public DbSet<BrandMst> BrandMsts { get; set; }
+        public DbSet<CatMst> CatMsts { get; set; }
+        public DbSet<CertifyMst> CertifyMsts { get; set; }
+        public DbSet<DimMst> DimMsts { get; set; }
+        public DbSet<DimQltyMst> DimQltyMsts { get; set; }
+        public DbSet<GoldKrtMst> GoldKrtMsts { get; set; }
+        public DbSet<ProdMst> ProdMsts { get; set; }
+        public DbSet<StoneMst> StoneMsts { get; set; }
+        public DbSet<StoneQltyMst> StoneQltyMsts { get; set; }
+        public DbSet<ItemMst> Items { get; set; }
+        public DbSet<DimInfoMst> DimInfos { get; set; }
+        public DbSet<JewelTypeMst> JewelTypes { get; set; }
+        public DbSet<DimQltySubMst> DimQltySubMsts { get; set; }
+        public DbSet<User> User { get; set; }
 
 
-        modelBuilder.Entity<DimInfoMst>()
-            .HasKey(d => d.DimID);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ItemMst>()
+              .HasKey(i => i.Style_Code);
 
-        // Thêm các quan hệ và cấu hình khóa ngoại khác tại đây
+            modelBuilder.Entity<ItemMst>()
+                .HasOne(i => i.Brand)
+                .WithMany()
+                .HasForeignKey(i => i.Brand_ID);
 
-        base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ItemMst>()
+                .HasOne(i => i.Category)
+                .WithMany()
+                .HasForeignKey(i => i.Cat_ID);
+
+            modelBuilder.Entity<ItemMst>()
+                .HasOne(i => i.Certify)
+                .WithMany()
+                .HasForeignKey(i => i.Certify_ID);
+
+            modelBuilder.Entity<ItemMst>()
+                .HasOne(i => i.Product)
+                .WithMany()
+                .HasForeignKey(i => i.Prod_ID);
+
+            modelBuilder.Entity<ItemMst>()
+                .HasOne(i => i.GoldCarat)
+                .WithMany()
+                .HasForeignKey(i => i.GoldType_ID);
+            modelBuilder.Entity<DimMst>()
+                .HasOne(d => d.Item)
+                .WithMany(i => i.Dimensions)
+                .HasForeignKey(d => d.Style_Code);
+
+
+            modelBuilder.Entity<DimInfoMst>()
+                .HasKey(d => d.DimID);
+
+            // Thêm các quan hệ và cấu hình khóa ngoại khác tại đây
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
